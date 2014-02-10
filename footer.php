d615b80ce1372fd3b318293b4cf12b54afbf05c8
Jack <jack@jack-l.com>
2014-02-09 19:27:59 -0700

    Removed test files, uploaded template files
    Removed test files, uploaded template files. Get ready for us to start
    work on stuff

---
+++ footer.php
@@ -0,0 +1,19 @@
+	<!-- footer -->
+	<footer>
+		<div class="container_16">
+			<nav>
+				<ul>
+					<li><a href="index.html">home</a></li>
+					<li><a href="index-1.html">Selling</a></li>
+					<li><a href="index-2.html">buying</a></li>
+					<li><a href="index-3.html">renting</a></li>
+					<li><a href="index-4.html">moving</a></li>
+					<li><a href="index-5.html">finance</a></li>
+					<li><a href="index-6.html">contacts</a></li>		
+				</ul>
+			</nav>
+		</div>
+	</footer>
+	<script type="text/javascript"> Cufon.now(); </script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="template/css/reset.css" type="text/css" media="all">
+	<link rel="stylesheet" href="template/css/grid.css" type="text/css" media="all">
+	<link rel="stylesheet" href="template/css/style.css" type="text/css" media="all">
+	<script type="text/javascript" src="template/js/jquery-1.4.2.min.js" ></script>
+	<script type="text/javascript" src="template/js/cufon-yui.js"></script>
+	<script type="text/javascript" src="template/js/Myriad_Pro_italic_400-Myriad_Pro_italic_600.font.js"></script>
+	<script type="text/javascript" src="template/js/cufon-replace.js"></script>	
+	<script type="text/javascript" src="template/js/jquery.faded.js"></script>
+	<script type="text/javascript" src="template/js/script.js"></script>
+	<!--[if lt IE 7]>
+		<link rel="stylesheet" href="template/css/ie/ie6.css" type="text/css" media="screen">
+		<script type="text/javascript" src="template/js/ie_png.js"></script>
+		<script type="text/javascript">
+				ie_png.fix('.png, .logo, .extra-banner');
+		</script>
+	<![endif]-->
+	<!--[if lt IE 9]>
+		<script type="text/javascript" src="template/js/html5.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<!-- header -->
+	<header>
+		<div class="container_16">
+			<div class="logo">
+				<h1><a href="index.html"><strong>Real</strong> Estate</a></h1>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.html">Home</a></li>
+					<li><a href="index-1.html" class="current">Selling</a></li>
+					<li><a href="index-2.html">Buying</a></li>
+					<li><a href="index-3.html">Renting</a></li>
+					<li><a href="index-4.html">Moving</a></li>
+					<li><a href="index-5.html">Finance</a></li>
+					<li><a href="index-6.html">Contacts</a></li>
+				</ul>
+			</nav>
