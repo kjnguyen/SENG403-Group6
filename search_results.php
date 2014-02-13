ba38a4a12e1c4d9bd6a07d9bb1a993a1b78152ee
xiaolin99 <xiao.lin99@gmail.com>
2014-02-12 18:06:31 -0700

    Xiao updated search pages

--- search_results.php
+++ search_results.php
@@ -1,8 +1,42 @@
 <?php
+    include_once "header.php";
+?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+	<!-- content -->
+	<section id="content">
+		<div class="container_16">
+			<div class="clearfix">
+				<section id="mainContent" class="grid_10">
+					<article>
+						<h2>Xiao's Search Results Page</h2>
+                                                <?php
+                                                    include_once 'mysqlcon.php';
+                                                    $con = getSQLConnection();
+                                                    mysqli_select_db($con, 's403_project');
+                                                    //mysqli_connect("localhost", "root", "1234", "s403_project");
+                                                    echo mysqli_error($con);
+                                                    echo "hello, this is displaying from the live Company table <br>";
+                                                    $query = "select * from Company";
+                                                    $results = mysqli_query($con, $query);
+                                                    echo mysqli_error($con);
+                                                    while ($row = mysqli_fetch_assoc($results)) {
+                                                        echo $row['name'];
+                                                        echo ' | ';
+                                                        echo $row['address'];
+                                                        echo ' | ';
+                                                        echo $row['desription'];
+                                                        echo '<br>';
+                                                    }
+                     
+                                                    echo mysqli_error($con);
+                                                ?>
+						<h3>Search Results</h3>
+					</article>
+				</section>
+			</div>
+		</div>
+	</section>
 
+<?php
+    include_once "footer.php";
+?>
