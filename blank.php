8cdf9eb3fb12e2bec4d5ec2b2b46909bc3cb7ff7
Jordan Gay <jcgay3@gmail.com>
2014-02-12 18:09:54 -0700

    Update blank.php

--- blank.php
+++ blank.php
@@ -8,8 +8,13 @@
 			<div class="clearfix">
 				<section id="mainContent" class="grid_10">
 					<article>
-						<h2>WELCOME</h2>
-						<h3>H3</h3>
+						<h2>Welcome to 4J Estates Online Real Estate Catalog!</h2>
+						<h3>For realtors, as well as prospective buyers and renters who
+						 are looking for a convenient marketplace; 4J Estates is a website
+						  that is fast, effective, and simple to use where realtors can
+						   advertise properties and where buyers can easily find their 
+						   dream estate. Unlike other real estate markets, our product
+						    is easily accessible, user friendly, and ad-free!</h3>
 					</article>
 				</section>
 			</div>
