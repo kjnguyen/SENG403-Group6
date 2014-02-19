6dc736c85b0a7b512c18bd79b3126870c0081b48
Jordan Gay <jcgay3@gmail.com>
2014-02-19 10:58:16 -0700

    Update index.php

--- index.php
+++ index.php
@@ -9,12 +9,12 @@
 				<section id="mainContent" class="grid_10">
 					<article>
 						<h2>Welcome to 4J Estates Online!</h2>
-						<h3>For realtors, as well as prospective buyers and renters who
+						<p>For realtors, as well as prospective buyers and renters who
 						 are looking for a convenient marketplace; 4J Estates is a website
 						  that is fast, effective, and simple to use where realtors can
 						   advertise properties and where buyers can easily find their 
 						   dream estate. Unlike other real estate markets, our product
-						    is easily accessible, user friendly, and ad-free!</h3>
+						    is easily accessible, user friendly, and ad-free!</p>
 					</article>
 				</section>
 			</div>
