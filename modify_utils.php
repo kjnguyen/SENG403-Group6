0fbcc548d87833883bcaeab84582e0f7afe5416b
Jordan Gay <jcgay3@gmail.com>
2014-02-22 20:00:23 -0700

    Update modify_utils.php

--- modify_utils.php
+++ modify_utils.php
@@ -27,7 +27,10 @@ function modify_values($id, $CompID, $price, $sq_ft, $num_floors,
             . "num_bdrm='$num_bdrm', num_baths='$num_baths', year_built='$year_built', prop_type='$prop_type',"
             . "bldg_type='$bldg_type', district='$district', maintenance='$maintenance', status='$status',"
             . "address='$address', description='$description' WHERE ID='$id'" ;*/
-            
+   if(!$id){
+       return $message = "ID is required";
+   }     
+   
    $sql = "UPDATE listing SET price='$price' WHERE ID='$id'";        
    $result = mysqli_query($con, $sql);
 
