7d90d1bc2ac78509ebcac6b6422f1cedb34d5c08
Jordan Gay <jcgay3@gmail.com>
2014-02-22 22:38:02 -0700

    Update modify_utils.php

--- modify_utils.php
+++ modify_utils.php
@@ -22,16 +22,17 @@ function modify_values($id, $CompID, $price, $sq_ft, $num_floors,
   
     $con = getSQLConnection();
     mysqli_select_db($con, 's403_project');
-    
-   /* $sql = "UPDATE Listing SET CompID='$CompID', price='$price', sq_ft='$sq_ft', num_floors='$num_floors', "
-            . "num_bdrm='$num_bdrm', num_baths='$num_baths', year_built='$year_built', prop_type='$prop_type',"
-            . "bldg_type='$bldg_type', district='$district', maintenance='$maintenance', status='$status',"
-            . "address='$address', description='$description' WHERE ID='$id'" ;*/
+   
    if(!$id){
        return $message = "ID is required";
    }     
+    $sql = "UPDATE Listing SET CompID='$CompID', price='$price', sq_ft='$sq_ft', num_floors='$num_floors', "
+            . "num_bdrm='$num_bdrm', num_baths='$num_baths', year_built='$year_built', prop_type='$prop_type',"
+            . "bldg_type='$bldg_type', district='$district', maintenance='$maintenance', status='$status',"
+            . "address='$address', description='$description' WHERE ID='$id'" ;
+    
    
-   $sql = "UPDATE Listing SET price='$price' WHERE ID='$id'";        
+  // $sql = "UPDATE Listing SET price='$price' WHERE ID='$id'";        
    $result = mysqli_query($con, $sql);
  
    mysqli_close($con);
