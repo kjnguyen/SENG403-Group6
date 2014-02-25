0015f2a127aa85bf8e2d19b992539fee772a9332
Jordan Gay <jcgay3@gmail.com>
2014-02-25 08:51:09 -0700

    Update bad_permission.php

--- admin/bad_permission.php
+++ admin/bad_permission.php
@@ -21,7 +21,7 @@
 			<div class="row-fluid sortable">
 				<div class="box span12">
 					<div class="box-header well" data-original-title>
-						<h2><i class="icon-picture"></i>Blank</h2>
+						<h2><i class="icon-picture"></i>Permission Denied</h2>
 						<div class="box-icon">
 							<a href="#" class="btn btn-setting btn-round"><i class="icon-cog"></i></a>
 							<a href="#" class="btn btn-minimize btn-round"><i class="icon-chevron-up"></i></a>
@@ -29,7 +29,7 @@
 						</div>
 					</div>
 					<div class="box-content">
-						
+					<?php echo"Sorry you don't have permission to edit this Listing"; ?>	
 					</div>
 				</div><!--/span-->
 			
