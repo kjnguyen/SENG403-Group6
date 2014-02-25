872ddf5c3cf77878b3d3d7d0ecdebbf478b9cbc0
xiaolin99 <xiao.lin99@gmail.com>
2014-02-25 12:34:16 -0700

    Xiao updated creat listing

--- admin/postlistingfunc.php
+++ admin/postlistingfunc.php
@@ -7,18 +7,32 @@ if(!defined("postlisting.php"))
 }
 
 define("mysqlcon.php", True);
-include_once 'mysqlcon.php'
+include_once 'mysqlcon.php';
 
-function postlisting($compID, $price, $sq_ft, $num_floors, $num_bdrms, $year_built, $prop_type, $bldg_type, $district, $city, $maintenance_fee, $status, $num_baths, $address, $desription){
+function postlisting( $compID, 
+        $price, 
+        $sq_ft, 
+        $num_floors, 
+        $num_bdrms, 
+        $year_built, 
+        $prop_type, 
+        $bldg_type, 
+        $district, 
+        $city, 
+        $maintenance_fee, 
+        $status, 
+        $num_baths, 
+        $address, 
+        $description){
 
-$con = getSQLConnection();
-mysqli_select_db($con, 's403_project');
+    $con = getSQLConnection();
 
-static $ID = 0;
+    mysqli_select_db($con, 's403_project');
 
-$sql = "INSERT INTO Listing (ID, compID, price, sq_ft, num_floors, num_bdrms, year_built, prop_type, bldg_type, district, city, maintenance_fee, status, num_baths, address, description) VALUES ($ID, $compID, $price, $sq_ft, $num_floors, $num_bdrms, $year_built, $prop_type, $bldg_type, $district, $city, $maintenance_fee, $status, $num_baths, $address, $description)";
-$result = mysqli_query($con, $sql);
 
-$ID ++;
+    $sql = "INSERT INTO Listing (compID, price, sq_ft, num_floors, num_bdrms, year_built, prop_type, bldg_type, district, city, maintenance_fee, status, num_baths, address, description) VALUES ($compID, $price, $sq_ft, $num_floors, $num_bdrms, $year_built, $prop_type, $bldg_type, $district, $city, $maintenance_fee, $status, $num_baths, $address, $description)";
+    $result = mysqli_query($con, $sql);
+
 
 }
+?>
