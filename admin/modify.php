d226679f5927ca49aa988a94030a1dae31a3eb1b
Jordan Gay <jcgay3@gmail.com>
2014-02-24 22:02:29 -0700

    Update modify.php

--- admin/modify.php
+++ admin/modify.php
@@ -32,24 +32,7 @@
 						<form name="Modify" method="get" action="modify_results.php" id="contacts-form" style="font-size: 80%;">
 							<fieldset>
 								<div class="control-group">
-							    <p><label>Listing ID: </label>
-								    <select name="ID">
-								        <?php
-								        define("modify_utils.php", True);
-								        include_once 'modify_utils.php';
-								        $id = get_list_of_id();
-								        if(!empty($id)) {
-								            $selected = $_GET['ID'];
-								            foreach ($id as $i) {
-								                echo "<option value=\"".$i['ID']."\"";
-								                if ($i['ID'] == $selected) {
-								                    echo " selected=\"selected\"";
-								                }
-								                echo ">".$i['ID']."</option>";
-								            }
-								        }
-								        ?>
-								    </select></p> 
+							    
 										 <?php
 											       // echo "
 											      // <p><label>ID number(required): </label><input type=\"text\" name=\"ID\" value=\"".$_GET['ID']."\"/></a></p>
