b9edbbc0c2d410f228f74c5a48016bb58cd16b68
Jordan Gay <jcgay3@gmail.com>
2014-02-25 09:32:49 -0700

    Update modify_utils.php

--- admin/modify_utils.php
+++ admin/modify_utils.php
@@ -202,7 +202,7 @@ function check_permission($id){
     if ($_SESSION['Authed_Permission'] == 2 ){
            $query = "SELECT CompID from Listing WHERE ID='$id'";
            $result = mysqli_query($con, $query);
-           $row = mysqli_fetch_assoc($results)
+           $row = mysqli_fetch_assoc($results);
            if ($_SESSION['Authed_UserID'] == $row ){
                return 1;
            }
