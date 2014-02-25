fd5eedca5e638b01aaed30c95c61401bdc56bb09
Jordan Gay <jcgay3@gmail.com>
2014-02-24 18:14:24 -0700

    Update modify.php

--- admin/modify.php
+++ admin/modify.php
@@ -21,7 +21,7 @@
 			<div class="row-fluid sortable">
 				<div class="box span12">
 					<div class="box-header well" data-original-title>
-						<h2><i class="icon-picture"></i>Blank</h2>
+						<h2><i class="icon-picture"></i>Modify Listing</h2>
 						<div class="box-icon">
 							<a href="#" class="btn btn-setting btn-round"><i class="icon-cog"></i></a>
 							<a href="#" class="btn btn-minimize btn-round"><i class="icon-chevron-up"></i></a>
@@ -38,7 +38,7 @@
 										value="Input Value Here"></input>
 									</div>
 									<div class="form-actions">
-										<button class="btn btn-primary" type="submit">Modify Listing</button>
+										<button class="btn btn-primary" type="submit">Modify</button>
 								</div>
 							</fieldset>
 						</form>
