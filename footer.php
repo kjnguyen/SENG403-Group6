a43eb487b02decc993cc4974ddbd277b01c0be00
xiaolin99 <xiao.lin99@gmail.com>
2014-02-24 17:28:18 -0700

    Xiao updated footer

--- footer.php
+++ footer.php
@@ -4,16 +4,12 @@
 			<nav>
 				<ul>
 					<li><a href="index.html">home</a></li>
-					<li><a href="index-1.html">Selling</a></li>
-					<li><a href="index-2.html">buying</a></li>
-					<li><a href="index-3.html">renting</a></li>
-					<li><a href="index-4.html">moving</a></li>
-					<li><a href="index-5.html">finance</a></li>
 					<li><a href="index-6.html">contacts</a></li>		
 				</ul>
 			</nav>
             <p class="copy">-</p>
 		</div>
+            <a href="http://all-free-download.com/free-website-templates/real_estate_template_2245.html">Powered by All-free-download.com (GNU GPL License)</a>
 	</footer>
 	<script type="text/javascript"> Cufon.now(); </script>
 </body>
