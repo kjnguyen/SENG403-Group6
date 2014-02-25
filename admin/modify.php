2f2a61f7d1f2226f8f4af14595fdb599b1494e70
Jordan Gay <jcgay3@gmail.com>
2014-02-25 09:09:07 -0700

    Update modify.php

--- admin/modify.php
+++ admin/modify.php
@@ -11,7 +11,7 @@
 				include_once 'modify_utils.php';
 				$permission = check_permission($_GET['ID']);
 				if ($permission != 1){
-					header('Location: http://www.s403.jack-l.com/admin/bad_permission.php');
+					printf("<script>location.href='http://www.s403.jack-l.com/admin/bad_permission.php'</script>");
 				}
 			?>
 			<div>
