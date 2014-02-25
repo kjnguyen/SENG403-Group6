23c57be586c203b291833f44de2bbbb986294508
Jordan Gay <jcgay3@gmail.com>
2014-02-24 18:19:01 -0700

    Update modify.php

--- admin/modify.php
+++ admin/modify.php
@@ -51,6 +51,28 @@
 									<div class="controls">
 										<input id="focuesInput" class="input-xlarge focused" type="text"
 										value="Input Value Here"></input>
+										 <?php
+											       // echo "
+											      // <p><label>ID number(required): </label><input type=\"text\" name=\"ID\" value=\"".$_GET['ID']."\"/></a></p>
+											      // <p><label>Company ID: </label><input type=\"text\" name=\"CompID\" value=\"".$_GET['CompID']."\"/></p>
+											
+											      echo "
+											<p><label>Company ID: </label><input type=\"text\" name=\"CompID\" value=\"".$_GET['CompID']."\"/></p>
+											<p><label>Price: </label><input type=\"text\" name=\"price\" value=\"".$_GET['price']."\"/></p>
+											<p><label>Square Feet: </label><input type=\"text\" name=\"sq_ft\" value=\"".$_GET['sq_ft']."\"/></p>
+											<p><label>Number of Floors: </label><input type=\"text\" name=\"num_floors\" value=\"".$_GET['num_floors']."\"/></p>
+											<p><label>Number of Bedrooms: </label><input type=\"text\" name=\"num_bdrm\" value=\"".$_GET['num_bdrm']."\"/></p>
+											<p><label>Number of Bathrooms: </label><input type=\"text\" name=\"num_baths\" value=\"".$_GET['num_baths']."\"/></p>
+											<p><label>Year Built: </label><input type=\"text\" name=\"year_built\" value=\"".$_GET['year_built']."\"/></p>
+											<p><label>Property Type: </label><input type=\"text\" name=\"prop_type\" value=\"".$_GET['prop_type']."\"/></p>
+											<p><label>Building Type: </label><input type=\"text\" name=\"bldg_type\" value=\"".$_GET['bldg_type']."\"/></p>
+											<p><label>District: </label><input type=\"text\" name=\"district\" value=\"".$_GET['district']."\"/></p>
+											<p><label>Maintenance: </label><input type=\"text\" name=\"maintenance_fee\" value=\"".$_GET['maintenance_fee']."\"/></p>
+											<p><label>Address: </label><input type=\"text\" name=\"address\" value=\"".$_GET['address']."\"/></p>
+											<p><label>Description: </label><input type=\"text\" name=\"description\" value=\"".$_GET['description']."\"/></p>
+											
+											"
+										 ?>
 									</div>
 									<div class="form-actions">
 										<button class="btn btn-primary" type="submit">Modify</button>
