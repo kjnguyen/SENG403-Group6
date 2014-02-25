5102c9f607e9441d68d697bcf3bac83b59d5ebef
Jordan Gay <jcgay3@gmail.com>
2014-02-24 19:32:16 -0700

    Update modify_results.php

--- admin/modify_results.php
+++ admin/modify_results.php
@@ -1,67 +1,159 @@
+<?php
+//Check user login
+    include_once 'auth_utils.php';
+    if(!isset($no_visible_elements) || !$no_visible_elements) {
+        CheckLogin();
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title></title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="template/css/reset.css" type="text/css" media="all">
-	<link rel="stylesheet" href="template/css/grid.css" type="text/css" media="all">
-	<link rel="stylesheet" href="template/css/style.css" type="text/css" media="all">
-	<script type="text/javascript" src="template/js/jquery-1.4.2.min.js" ></script>
-	<script type="text/javascript" src="template/js/cufon-yui.js"></script>
-	<script type="text/javascript" src="template/js/Myriad_Pro_italic_400-Myriad_Pro_italic_600.font.js"></script>
-	<script type="text/javascript" src="template/js/cufon-replace.js"></script>	
-	<script type="text/javascript" src="template/js/jquery.faded.js"></script>
-	<script type="text/javascript" src="template/js/script.js"></script>
-	<!--[if lt IE 7]>
-		<link rel="stylesheet" href="template/css/ie/ie6.css" type="text/css" media="screen">
-		<script type="text/javascript" src="template/js/ie_png.js"></script>
-		<script type="text/javascript">
-				ie_png.fix('.png, .logo, .extra-banner');
-		</script>
-	<![endif]-->
+	<title>4J Estates</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Charisma, a fully featured, responsive, HTML5, Bootstrap admin template.">
+	<meta name="author" content="Muhammad Usman">
+
+	<!-- The styles -->
+	<link id="bs-css" href="css/bootstrap-cerulean.css" rel="stylesheet">
+	<style type="text/css">
+	  body {
+		padding-bottom: 40px;
+	  }
+	  .sidebar-nav {
+		padding: 9px 0;
+	  }
+	</style>
+	<link href="css/bootstrap-responsive.css" rel="stylesheet">
+	<link href="css/charisma-app.css" rel="stylesheet">
+	<link href="css/jquery-ui-1.8.21.custom.css" rel="stylesheet">
+	<link href='css/fullcalendar.css' rel='stylesheet'>
+	<link href='css/fullcalendar.print.css' rel='stylesheet'  media='print'>
+	<link href='css/chosen.css' rel='stylesheet'>
+	<link href='css/uniform.default.css' rel='stylesheet'>
+	<link href='css/colorbox.css' rel='stylesheet'>
+	<link href='css/jquery.cleditor.css' rel='stylesheet'>
+	<link href='css/jquery.noty.css' rel='stylesheet'>
+	<link href='css/noty_theme_default.css' rel='stylesheet'>
+	<link href='css/elfinder.min.css' rel='stylesheet'>
+	<link href='css/elfinder.theme.css' rel='stylesheet'>
+	<link href='css/jquery.iphone.toggle.css' rel='stylesheet'>
+	<link href='css/opa-icons.css' rel='stylesheet'>
+	<link href='css/uploadify.css' rel='stylesheet'>
+
+	<!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
-		<script type="text/javascript" src="template/js/html5.js"></script>
+	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
+
+	<!-- The fav icon -->
+	<link rel="shortcut icon" href="img/favicon.ico">
+		
 </head>
 
 <body>
-	<!-- header -->
-	<header>
-		<div class="container_16">
-			<div class="logo">
-				<h1><a href="index.html"><strong>Real</strong> Estate</a></h1>
-			</div>
-			<nav>
-				<ul>
-					<li><a href="index.php">Home</a></li>
-					<li><a href="index-1.html" class="current">Selling</a></li>
-					<li><a href="index-2.html">Buying</a></li>
-					<li><a href="index-3.html">Renting</a></li>
-					<li><a href="modify.php">Moving</a></li>
-					<li><a href="index-5.html">Finance</a></li>
-					<li><a href="index-6.html">Contacts</a></li>
-				</ul>
-			</nav>
-            
-            <!-- From Jack: You will have change the listing here...-->
-			<!-- faded slider begin -->
-			<div id="faded">
-				<div class="rap">
-					<a href="#"><img src="images/big-img1.jpg" alt="" width="571" height="398"></a>
-					<a href="#"><img src="images/big-img2.jpg" alt="" width="571" height="398"></a>
-					<a href="#"><img src="images/big-img3.jpg" alt="" width="571" height="398"></a>
+	<?php if(!isset($no_visible_elements) || !$no_visible_elements)	{ ?>
+	<!-- topbar starts -->
+	<div class="navbar">
+		<div class="navbar-inner">
+			<div class="container-fluid">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".top-nav.nav-collapse,.sidebar-nav.nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
+				<a class="brand" href="index.html"> <img alt="Charisma Logo" src="img/logo20.png" /> <span>Charisma</span></a>
+				
+				<!-- theme selector starts -->
+				<div class="btn-group pull-right theme-container" >
+					<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+						<i class="icon-tint"></i><span class="hidden-phone"> Change Theme / Skin</span>
+						<span class="caret"></span>
+					</a>
+					<ul class="dropdown-menu" id="themes">
+						<li><a data-value="classic" href="#"><i class="icon-blank"></i> Classic</a></li>
+						<li><a data-value="cerulean" href="#"><i class="icon-blank"></i> Cerulean</a></li>
+						<li><a data-value="cyborg" href="#"><i class="icon-blank"></i> Cyborg</a></li>
+						<li><a data-value="redy" href="#"><i class="icon-blank"></i> Redy</a></li>
+						<li><a data-value="journal" href="#"><i class="icon-blank"></i> Journal</a></li>
+						<li><a data-value="simplex" href="#"><i class="icon-blank"></i> Simplex</a></li>
+						<li><a data-value="slate" href="#"><i class="icon-blank"></i> Slate</a></li>
+						<li><a data-value="spacelab" href="#"><i class="icon-blank"></i> Spacelab</a></li>
+						<li><a data-value="united" href="#"><i class="icon-blank"></i> United</a></li>
+					</ul>
 				</div>
-				<ul class="pagination" style="right: auto; left: 600px;">
-                                    <?php
-                                    define("search.php", True);
-                                    include_once 'search.php';
-                                    ?>
-				</ul>
-				<img src="images/extra-banner.png" alt="" class="extra-banner">
+				<!-- theme selector ends -->
+				
+				<!-- user dropdown starts -->
+				<div class="btn-group pull-right" >
+					<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+						<i class="icon-user"></i><span class="hidden-phone"> <?php echo $_SESSION['Authed_Username'];?></span>
+						<span class="caret"></span>
+					</a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Profile</a></li>
+						<li class="divider"></li>
+						<li><a href="./logout.php">Logout</a></li>
+					</ul>
+				</div>
+				<!-- user dropdown ends -->
+				
+				<div class="top-nav nav-collapse">
+					<ul class="nav">
+						<li><a href="#">Visit Site</a></li>
+						<li>
+							<form class="navbar-search pull-left">
+								<input placeholder="Search" class="search-query span2" name="query" type="text">
+							</form>
+						</li>
+					</ul>
+				</div><!--/.nav-collapse -->
 			</div>
-			<!-- faded slider end -->
 		</div>
-	</header>
+	</div>
+	<!-- topbar ends -->
+	<?php } ?>
+	<div class="container-fluid">
+		<div class="row-fluid">
+		<?php if(!isset($no_visible_elements) || !$no_visible_elements) { ?>
+		
+			<!-- left menu starts -->
+			<div class="span2 main-menu-span">
+				<div class="well nav-collapse sidebar-nav">
+					<ul class="nav nav-tabs nav-stacked main-menu">
+						<li class="nav-header hidden-tablet">Main</li>
+						<li><a class="ajax-link" href="index.html"><i class="icon-home"></i><span class="hidden-tablet"> Dashboard</span></a></li>
+						<li><a class="ajax-link" href="ui.html"><i class="icon-eye-open"></i><span class="hidden-tablet"> UI Features</span></a></li>
+						<li><a class="ajax-link" href="form.html"><i class="icon-edit"></i><span class="hidden-tablet"> Forms</span></a></li>
+						<li><a class="ajax-link" href="chart.html"><i class="icon-list-alt"></i><span class="hidden-tablet"> Charts</span></a></li>
+						<li><a class="ajax-link" href="typography.html"><i class="icon-font"></i><span class="hidden-tablet"> Typography</span></a></li>
+						<li><a class="ajax-link" href="gallery.html"><i class="icon-picture"></i><span class="hidden-tablet"> Gallery</span></a></li>
+						<li class="nav-header hidden-tablet">Sample Section</li>
+						<li><a class="ajax-link" href="table.html"><i class="icon-align-justify"></i><span class="hidden-tablet"> Tables</span></a></li>
+						<li><a class="ajax-link" href="calendar.html"><i class="icon-calendar"></i><span class="hidden-tablet"> Calendar</span></a></li>
+						<li><a class="ajax-link" href="grid.html"><i class="icon-th"></i><span class="hidden-tablet"> Grid</span></a></li>
+						<li><a class="ajax-link" href="file-manager.html"><i class="icon-folder-open"></i><span class="hidden-tablet"> File Manager</span></a></li>
+						<li><a href="tour.html"><i class="icon-globe"></i><span class="hidden-tablet"> Tour</span></a></li>
+						<li><a class="ajax-link" href="icon.html"><i class="icon-star"></i><span class="hidden-tablet"> Icons</span></a></li>
+						<li><a href="error.html"><i class="icon-ban-circle"></i><span class="hidden-tablet"> Error Page</span></a></li>
+						<li><a href="login.html"><i class="icon-lock"></i><span class="hidden-tablet"> Login Page</span></a></li>
+					</ul>
+					<label id="for-is-ajax" class="hidden-tablet" for="is-ajax"><input id="is-ajax" type="checkbox"> Ajax on menu</label>
+				</div><!--/.well -->
+			</div><!--/span-->
+			<!-- left menu ends -->
+			
+			<noscript>
+				<div class="alert alert-block span10">
+					<h4 class="alert-heading">Warning!</h4>
+					<p>You need to have <a href="http://en.wikipedia.org/wiki/JavaScript" target="_blank">JavaScript</a> enabled to use this site.</p>
+				</div>
+			</noscript>
+			
+			<div id="content" class="span10">
+			<!-- content starts -->
+			<?php } ?>
 
 	<!-- content -->
 	<section id="content">
