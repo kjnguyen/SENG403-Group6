9d1ad2726a690bbb4bfd43070fefe1448f755187
johnm7115 <johnm7115@gmail.com>
2014-02-25 16:17:07 -0700

    Update pictures.php
    Testing.

--- admin/pictures.php
+++ admin/pictures.php
@@ -9,8 +9,6 @@ $con = getSQLConnection();
 if(!mysqli_errno($con))
 {
   $pictureList = getPictures($con, $ID);
-  
-  
 }
 
 $_SESSION["token"] = $token = uniqid(rand(), true);
@@ -39,9 +37,31 @@ $_SESSION["token"] = $token = uniqid(rand(), true);
   
   
 </script>
-
+<!--
 <div class="control-group">
   <label class="control-label" for="fileInput">Add Picture</label>
+  <div class="controls">
+    <input class="input-file uniform_on" id="fileInput" type="file" name="file">
+  </div>
+</div>
+<input type="hidden" name="token" value="<?php echo $token; ?>" /> -->
+
+<div class="control-group">
+  <legend>Edit Pictures</legend>
+  <table class="table table-striped">
+  <?php echo "
+    <tr>
+      <td>";
+      echo "Test Picture";
+      echo '</td>
+      <td><a class="btn btn-danger" href="#">
+          <i class="icon-trash icon-white"></i> 
+          Delete
+        </a>
+      </td>
+    </tr>';
+  ?>
+  </table>
   <div class="controls">
     <input class="input-file uniform_on" id="fileInput" type="file" name="file">
   </div>
