62b508553dbf0e713e986332e997c7d29e496b67
Jordan Gay <jcgay3@gmail.com>
2014-02-24 17:50:24 -0700

    Update modify.php

--- admin/modify.php
+++ admin/modify.php
@@ -19,17 +19,22 @@
 			</div>
 
 			<div class="row-fluid sortable">
-				<div class="box span12">
-					<div class="box-header well" data-original-title>
-						<h2><i class="icon-picture"></i>Blank</h2>
-						<div class="box-icon">
-							<a href="#" class="btn btn-setting btn-round"><i class="icon-cog"></i></a>
-							<a href="#" class="btn btn-minimize btn-round"><i class="icon-chevron-up"></i></a>
-							<a href="#" class="btn btn-close btn-round"><i class="icon-remove"></i></a>
-						</div>
-						<?php echo"TEST1" ?>
-					</div>
-					<div class="box-content">
+			<fieldset>
+
+			    <div class="control-group"></div>
+			    <div class="control-group"></div>
+			    <div class="control-group"></div>
+			    <div class="control-group"></div>
+			    <div class="control-group warning"></div>
+			    <div class="control-group error"></div>
+			    <div class="control-group success"></div>
+			    <div class="control-group"></div>
+			    <div class="control-group"></div>
+			    <div class="control-group"></div>
+			    <div class="control-group"></div>
+			    <div class="form-actions"></div>
+			
+			</fieldset>
 						
 					</div>
 				</div><!--/span-->
