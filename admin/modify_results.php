c66a9a3c6943719ecfc1519c26bf5f24d3227d93
Jordan Gay <jcgay3@gmail.com>
2014-02-24 19:36:11 -0700

    Update modify_results.php

--- admin/modify_results.php
+++ admin/modify_results.php
@@ -155,104 +155,38 @@
 			<!-- content starts -->
 			<?php } ?>
 
-	<!-- content -->
-	<section id="content">
-		<div class="container_16">
-			<div class="clearfix">
-				<section id="mainContent" class="grid_10" style="position: relative; width: 100%;">
-					<article style="position: relative; width: 100%;">
-						<h2>Modify listing</h2>
-                                               
-                                               
-                                                <?php
-                                                 
-                                                    define("modify_utils.php", True);
-                                                    include_once 'modify_utils.php';
-                                              
-                                                
-                                                    $id = $_GET['ID'];
-                                                    $CompID = $_GET['CompID'];
-                                                    $price = $_GET['price'];
-                                                    $sq_ft = $_GET['sq_ft'];                         
-                                                    $num_floors = $_GET['num_floors'];
-                                                    $num_bdrm = $_GET['num_bdrm'];
-                                                    $num_baths = $_GET['num_baths'];
-                                                    $year_built = $_GET['year_built'];
-                                                    $prop_type = $_GET['prop_type'];
-                                                    $bldg_type = $_GET['bldg_type'];
-                                                    $district = $_GET['district'];
-                                                    $maintenance_fee = $_GET['maintenance_fee'];
-                                                    $status = $_GET['status'];  
-                                                    $address = $_GET['address'];
-                                                    $description = $_GET['description'];
-                                                     
-                                                    
-                                                  /*  mysql_connect("$host","$user", "$pass")or die("cannot connect");  
-                                                    mysql_select_db("$db")or die("cannot select DB");  */
-                                                    
+<!-- *****************************************************************************-->
 
-                                                    $results_array = modify_values($id,$CompID, $price, $sq_ft, $num_floors, 
-                                                            $num_bdrm, $num_baths, $year_built, $prop_type, $bldg_type, 
-                                                            $district, $maintenance_fee, $status, $address, $description);
-                                                    if($results_array){
-                                                    	echo "Error: An $results_array<br>";
-                                                    }	
-                                                    else{
-	                                                    echo"Modifictions made";
-	                                                    echo "<br>Values that were updated:";
-	                                                    if($CompID){	
-	    						    	echo "<br>Company ID: $CompID";
-	                                                    }
-	                                                    if($price){
-	    						    	echo "<br>Price: $price";
-	                                                    }
-	                                                    if($sq_ft){
-	    						    	echo "<br>Square Footage: $sq_ft";
-	                                                    }
-	                                                    if($num_floors){
-	    						    	echo "<br>Number of Floors: $num_floors";
-	                                                    }
-	                                                    if($num_bdrm){
-	    						    	echo "<br>Number of Bedrooms: $num_bdrm";
-	                                                    }
-	                                                    if($num_baths){
-	    							 echo "<br>Number of Bathrooms: $num_baths";
-	                                                    }
-	                                                    if($year_built){
-	    						    	echo "<br>Year Built: $year_built";
-	                                                    }
-	                                                    if($prop_type){
-	    						    	echo "<br>Property Type: $prop_type";
-	                                                    }
-	                                                    if($bldg_type){
-	    						    	echo "<br>Building Type: $bldg_type";
-	                                                    }
-	                                                    if($district){
-	    						    	echo "<br>district: $district";
-	                                                    }
-	                                                    if($maintenance_fee){
-	    						    	echo "<br>Maintenance Fees: $maintenance_fee";
-	                                                    }
-	                                                    if($status){
-	    						    	echo "<br>Status: $status";
-	                                                    }
-	                                                    if($address){
-	    						    	echo "<br>Address: $address";
-	                                                    }
-	                                                    if($description){
-	    						    	echo "<br>Description: $description";
-	                                                    }
-                                                    }
-    				
-                                                ?>
-                                                
-						
-                            		
-					</article>
-				</section>
+			<div>
+				<ul class="breadcrumb">
+					<li>
+						<a href="#">Home</a> <span class="divider">/</span>
+					</li>
+					<li>
+						<a href="#">Blank</a>
+					</li>
+				</ul>
 			</div>
-		</div>
-	</section>
+
+			<div class="row-fluid sortable">
+				<div class="box span12">
+					<div class="box-header well" data-original-title>
+						<h2><i class="icon-picture"></i>Blank</h2>
+						<div class="box-icon">
+							<a href="#" class="btn btn-setting btn-round"><i class="icon-cog"></i></a>
+							<a href="#" class="btn btn-minimize btn-round"><i class="icon-chevron-up"></i></a>
+							<a href="#" class="btn btn-close btn-round"><i class="icon-remove"></i></a>
+						</div>
+					</div>
+					<div class="box-content">
+						
+					</div>
+				</div><!--/span-->
+			
+			</div><!--/row-->
+
+
+
 <?php
     include_once "footer.php";
 ?>
