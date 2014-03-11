5177e4a44a66d7e3858660b484675aa2e976ad53
jackl92 <jack@jack-l.com>
2014-03-11 17:26:34 -0600

    lalalal
    lalalala

---
+++ langs/lang_en_admin_general.php
@@ -0,0 +1,16 @@
+<?php
+   /*
+    * This file was written by Jack L (http://jack-l.com)
+    * Please do not change without my permission!
+    * You may use any part of the follow code on your other projects or programs.
+    * Please send me Email if you have any questions related to this page.
+    */
+
+    //Prevent Direct Access, return 404 page not found
+    if(!defined("INCLUDE_FILE"))
+    {
+    	header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found', true, 404);die();
+    }
+    
+    //Blank language
+?>
--- load_lang_util.php
+++ load_lang_util.php
@@ -1,11 +1,4 @@
 <?php
-    /*
-    * This file was written by Jack L (http://jack-l.com)
-    * Please do not change without my permission!
-    * You may use any part of the follow code on your other projects or programs.
-    * Please send me Email if you have any questions related to this page.
-    */
-
     //Prevent Direct Access, return 404 page not found
     if(!defined("INCLUDE_FILE"))
     {
@@ -20,8 +13,9 @@
     if (!isset($CurrentPageToRootDir)) { //If it is unset, assume the file is located at root folder
         $CurrentPageToRootDir = '.';
     }
-    if (!file_exists($CurrentPageToRootDir.'/langs/lang_'.$Cur_LangID.'_'.basename(__FILE__))) {$Cur_LangID = "en";}
-    if (!file_exists($CurrentPageToRootDir.'/langs/lang_'.$Cur_LangID.'_'.basename(__FILE__))) {die("Fatal Error: Unable to load language files.");}
+    echo basename(__FILE__).'<br />!';
+    if (!file_exists($CurrentPageToRootDir.'/langs/lang_'.$Cur_LangID.'_admin_general.php') || !file_exists($CurrentPageToRootDir.'/langs/lang_'.$Cur_LangID.'_'.basename(__FILE__))) {$Cur_LangID = "en";}
+    if (!file_exists($CurrentPageToRootDir.'/langs/lang_'.$Cur_LangID.'_admin_general.php') || !file_exists($CurrentPageToRootDir.'/langs/lang_'.$Cur_LangID.'_'.basename(__FILE__))) {die("Fatal Error: Unable to load language files.");}
     //Import language files
     include_once ($CurrentPageToRootDir.'/langs/lang_'.$Cur_LangID.'_'.basename(__FILE__));
 ?>
