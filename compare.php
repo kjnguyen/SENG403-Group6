f0c273e09adf9ca4841a0d2cd23760d39df74964
xiaolin99 <xiao.lin99@gmail.com>
2014-03-13 10:48:35 -0600

    Xiao hates CSS

--- compare.php
+++ compare.php
@@ -9,6 +9,26 @@ include_once "header.php";
 <div class="clearfix">
 <section id="mainContent" class="grid_10" style="position: relative; width: 100%;">
 <article style="position: relative; width: 100%;">
+<style>
+table,th,td
+{
+border:2px solid red;
+border-collapse:collapse;
+padding:5px;
+}
+th,td
+{
+padding:5px;
+}
+th
+{
+text-align:left;
+}
+table
+{
+border-spacing:15px;
+}
+</style>
 <?php
 /* 
  * To change this license header, choose License Headers in Project Properties.
@@ -32,13 +52,45 @@ if ($itemCount == 0) {
     die();
 }
 
+$fields_map = array(
+    'price' => 'Price($)',
+    'date_listed' => 'Lising Date',
+    'sq_ft' => 'Area(sqft)',
+    'num_floors' => 'Floors',
+    'num_bdrms' => 'Bedrooms',
+    'num_baths' => 'Baths',
+    'year_built' => 'Built in',
+    'prop_type' => 'Property Type',
+    'bldg_type' => 'Building Type',
+    'district' => 'District',
+    'maintenance_fee' => 'Maitenance($)',
+    'status' => 'Status',
+    'address' => 'Address',
+    'c_name' => 'Company'
+    
+);
 
 
-
-echo '<table>';
-
+echo '<table style="width:500px">';
+echo '<tr>';
+echo '<th>Fields</th>';
+for ($i = 0; $i < $itemCount; $i ++) {
+    echo '<th>';
+    echo '<a href=item.php?ID="'.$compareItems[$i]['ID'].'" rel="0"><img src="images/f_thumb1.png" alt=""></a>';
+    echo '</th>';
+    unset($compareItems[$i]['ID']);
+    unset($compareItems[$i]['description']);
+    unset($compareItems[$i]['CompID']);
+    unset($compareItems[$i]['cityID']);
+    unset($compareItems[$i]['c_address']);
+    unset($compareItems[$i]['c_manager_name']);
+    unset($compareItems[$i]['c_phone_no']);
+}
+echo '</tr>';
 foreach ($compareItems[0] as $key => $item){
+    
     echo '<tr>';
+    echo '<td>'.$fields_map[$key].'</td>';
     for ($i = 0; $i < $itemCount; $i ++) {
         echo '<td>';
         echo $compareItems[$i][$key];
