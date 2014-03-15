5247b44994b256dd8a84e80990345581ae5ef6e3
Jordan Gay <jcgay3@gmail.com>
2014-03-15 14:20:11 -0600

    Update modify_company_utils.php

--- admin/modify_company_utils.php
+++ admin/modify_company_utils.php
@@ -35,11 +35,11 @@ function modify_values_secure($id, $name, $address, $manager_name, $phone_no, $d
    
     if(!$id){goto funcError;}  
       
-    $sql = "UPDATE Company SET name=? WHERE ID=?";
+    $sql = "UPDATE Company SET name=?, address=?, manager_name=? WHERE ID=?";
     //, address=?, manager_name=?, phone_no=?, decription=?
     echo $sql;
     if ($stmt = mysqli_prepare($con, $sql)) {
-        $stmt->bind_param('si', $name, $id);
+        $stmt->bind_param('sssi', $name, $address, $manager_name, $id);
         // $address, $manager_name, $phone_no, $description,
         if(!($stmt->execute())) {goto funcError;}
         $stmt->close();
