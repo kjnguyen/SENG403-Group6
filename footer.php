440b131f20da5b40f36f6fef6defa110728f0246
johnm7115 <johnm7115@gmail.com>
2014-03-15 00:43:33 -0600

    Fixed some links

--- footer.php
+++ footer.php
@@ -3,12 +3,12 @@
 		<div class="container_16">
 			<nav>
 				<ul>
-					<li><a href="index.html">home</a></li>
+					<li><a href="./">home</a></li>
                                         <li><a href="contact_info.html">contacts</a></li>		
 				</ul>
 			</nav>
                     <a href="http://all-free-download.com/free-website-templates/real_estate_template_2245.html">Template provided by All-free-download.com (GNU GPL License)</a>
-            <p class="copy">-</p>
+            <p class="copy"></p>
 		</div>
             
 	</footer>
