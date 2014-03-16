e7c9a339399593f1751dc9aa01ac15299d086f17
johnm7115 <johnm7115@gmail.com>
2014-03-16 10:56:52 -0600

    Added thumbnails to search results.

--- search_results.php
+++ search_results.php
@@ -9,46 +9,62 @@
 				<section id="mainContent" class="grid_10" style="position: relative; width: 100%;">
 					<article style="position: relative; width: 100%;">
 						<h2>Listing Search Results</h2>
-                                                <div id="faded" style="position: relative; width: 100%;">
-                                                <ul class="pagination" style="position: relative; top: 0px; left: 0px;">
-                                                <?php
-                                                    if(!defined("search_utils.php")) {define("search_utils.php", True);}
-                                                    include_once 'search_utils.php';
-                                                    
-                                                    $city = $_GET['city'];
-                                                    $province = $_GET['province'];
-                                                    $min_price = $_GET['min_price'];
-                                                    $max_price = $_GET['max_price'];
-                                                    $num_bdrm = $_GET['num_bdrm'];
-                                                    $district = $_GET['district'];
-                                                    $status = $_GET['status'];
-                                                    
-//                                                    echo "Search Criteria: <br>";
-//                                                    echo "city = $city, province = $province, min_price = $min_price, max_price = $max_price, num_bdrm = $num_bdrm, district  = $district, status = ".$status."<br>";
+                            <div id="faded" style="position: relative; width: 100%;">
+                                <ul class="pagination" style="position: relative; top: 0px; left: 0px;">
+                                <?php
+                                    if(!defined("search_utils.php")) {define("search_utils.php", True);}
+                                    include_once 'search_utils.php';
 
-                                                    $results_array = search_listing($city, $province, $min_price, $max_price, $num_bdrm, $district, $status);
-                                                    
-                                                    if (empty($results_array)) {
-                                                        echo "<h1>No Results Found</h1>";
-                                                    }
-                                                    else {
-                                                        foreach ($results_array as $row) {
-                                                            echo '<li class="" style="width: auto;">';
-                                                            echo '<a href="item.php?ID='.$row['ID'].'" rel="0">';
-                                                            echo '<img src="images/f_thumb1.png" alt="">';
-                                                            echo 'ID:   '.$row['ID'].'<br>';
-                                                            echo $row['sq_ft'].' Sqr. Ft.<br>';                                                                
-                                                            echo $row['num_bdrms'].' bedrooms<br>';
-//                                                            echo 'Date Listed:  '.$row['date_listed'].'<br>';
-                                                            echo 'Address:    '.$row['address'].'<br>';
-                                                            echo 'Price:    $'.$row['price'];
-                                                            echo '</a></li>';
-                                                        }
-                                                    }
-    
-                                                ?>
-                                                </ul>
-                                                </div>
+                                    $city = $_GET['city'];
+                                    $province = $_GET['province'];
+                                    $min_price = $_GET['min_price'];
+                                    $max_price = $_GET['max_price'];
+                                    $num_bdrm = $_GET['num_bdrm'];
+                                    $district = $_GET['district'];
+                                    $status = $_GET['status'];
+
+    //                                                    echo "Search Criteria: <br>";
+    //                                                    echo "city = $city, province = $province, min_price = $min_price, max_price = $max_price, num_bdrm = $num_bdrm, district  = $district, status = ".$status."<br>";
+
+                                    $results_array = search_listing($city, $province, $min_price, $max_price, $num_bdrm, $district, $status);
+
+                                    if (empty($results_array))
+                                    {
+                                        echo "<h1>No Results Found</h1>";
+                                    }
+                                    else
+                                    {
+                                        $con = getSQLConnection();
+                                        include_once './picturesLib.php';
+                                        
+                                        foreach ($results_array as $row)
+                                        {
+                                            $thumbs = getThumnails($con, $row['ID']);
+                                            
+                                            echo '<li class="" style="width: auto;">';
+                                            echo '<a href="item.php?ID='.$row['ID'].'" rel="0">';
+                                            if(!empty($thumbs))
+                                            {
+                                                echo '<img src="' . $thumbs[0]['path'] . '" height="84" width="93">';
+                                            }
+                                            else
+                                            {
+                                                echo '<img src="images/no-image.jpg" height="84" width="93">';
+                                            }
+                                            echo 'ID:   '.$row['ID'].'<br>';
+                                            echo $row['sq_ft'].' Sqr. Ft.<br>';                                                                
+                                            echo $row['num_bdrms'].' bedrooms<br>';
+    //                                        echo 'Date Listed:  '.$row['date_listed'].'<br>';
+                                            echo 'Address:    '.$row['address'].'<br>';
+                                            echo 'Price:    $'.$row['price'];
+                                            echo '</a></li>';
+                                        }
+                                        
+                                        mysqli_close($con);
+                                    }
+                                ?>
+                                </ul>
+                            </div>
 					</article>
 				</section>
 			</div>
