cfff18fa2f55f417ae67b25f55958d6a4a98ffb6
dtfsheedy <dtfsheedy@gmail.com>
2014-03-17 11:15:58 -0600

    fixing empcreate

--- admin/empcreate.php
+++ admin/empcreate.php
@@ -5,6 +5,13 @@
     //Do not output anything before this line (Do not use echo or html code)
     //---------------------------------------------------
 ?>
+
+<?php
+if ($_SESSION['Authed_Permission'] != 1) {
+    printf("<script>location.href='bad_permission.php'</script>");
+}
+?>
+
 <div>
 	<ul class="breadcrumb">
 		<li>
@@ -19,17 +26,14 @@
 <div class="row-fluid sortable">
 	<div class="box span12">
 		<div class="box-header well" data-original-title>
-			<h2><i class="icon-picture"></i>Create Employee Account</h2>
-				<div class="box-icon">
-					<a href="#" class="btn btn-setting btn-round"><i class="icon-cog"></i></a>
-					<a href="#" class="btn btn-minimize btn-round"><i class="icon-chevron-up"></i></a>
-					<a href="#" class="btn btn-close btn-round"><i class="icon-remove"></i></a>
-				</div>
+			<h2><i class="icon-edit"></i>Create Employee Account</h2>
+				
 		</div>
-	<div class="box-content">
-		<form class="form-horizontal" name=emp_create" method=post action"process_add_emp.php">
-		<fieldset>
-	<div class="control-group">
+		
+		<div class="box-content">
+		<form class="form-horizontal" name="emp_create" method="post" action="process_add_emp.php">
+			<fieldset>
+				<div class="control-group">
 						
 <label class="control-label" for"tyeahead">Agent Name</label>
 	<div class="controls">
@@ -61,7 +65,6 @@
 	</div>
 </div>
 
-	</div>
 	<input type="hidden" name="process_add_emp" value='true'>
 		<div class="form-actions">
 			<button type="submit" class="btn btn-primary">Create</button>
