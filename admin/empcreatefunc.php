722564697e978ca35ea40ee2b01238b1787d4de7
dtfsheedy <dtfsheedy@gmail.com>
2014-03-17 11:59:49 -0600

    working empcreatefunc

--- admin/empcreatefunc.php
+++ admin/empcreatefunc.php
@@ -9,21 +9,6 @@ if(!defined("empcreatefunc.php"))
 define("mysqlcon.php", True);
 include_once 'mysqlcon.php';
 
-
-function createemployee($compID, $agent_name, $phone_no, $email, $permission, $username, $password){
-
-$con = getSQLConnection();
-mysqli_select_db($con, 's403_project');
-
-static $empID = 0;
-
-$sql = "INSERT into Employee (empID, compID, agent_name, phone_no, email, permission, username, password) VALUES ($empID, $compID, $agent_name, $phone_no, $email, $permission, $username, $password)";
-$result = mysqli_query($con, $sql);
-
-$empID ++;
-
-}
-
 function create_emp_secure($compID, $agent_name, $phone_no, $email, $username, $raw_password) {
     
     $con = getSQLConnection();
