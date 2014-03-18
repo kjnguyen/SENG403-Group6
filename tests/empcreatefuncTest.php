dae87fac1706c7da73fe4e4bd92a099e9a00c3b1
kjnguyen <khajacknguyen@gmail.com>
2014-03-18 14:34:46 -0600

    Create empcreatefuncTest.php

---
+++ tests/empcreatefuncTest.php
@@ -0,0 +1,30 @@
+<?php
+
+include 'admin/empcreatefunc.php';
+
+/* ...EmptyInput() and ...ValidInput() fail 
+ * Because the function create_emp_secure(...) in empcreatefunc.php
+ * returns false no matter what. Nothing is added to database after function call
+ * Function is broken.
+ */
+
+class empcreatefuncTest extends PHPUnit_Framework_TestCase
+{
+	public function testCreateEmployee_EmptyInput()
+	{
+            $result = create_emp_secure(0, "", "", "", "", "");
+            $this->assertTrue($result);
+	}
+	
+	public function testCreateEmployee_NullInput()
+	{
+            $result = create_emp_secure(0, NULL, NULL, NULL, NULL, NULL);
+            $this->assertFalse($result);
+	}
+
+	public function testCreateEmployee_ValidInput()
+	{
+            $result = create_emp_secure(0, "Joanne Tester", "403-555-8378", "joannetest@testing.com", "joannatest", "testing");
+            $this->assertTrue($result);
+	}
+}
